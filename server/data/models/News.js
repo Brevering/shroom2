a0519b2e13266379c3b5a31408c2786432c1df9a
bbojkov <bbojkov90@googlemail.com>
2016-12-26 18:15:40 +0200

    Slight changes on the news mongoose model

--- server/data/models/News.js
+++ server/data/models/News.js
@@ -1,41 +1,66 @@
-const mongoose = require('mongoose');
+"use strict";
 
-module.exports.init = function () {
-    let newsSchema = mongoose.Schema({
-        title: {
+let mongoose = require("mongoose");
+
+let newsSchema = mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+        trim: true,
+        minlength: 5,
+        mexlength: 60
+    },
+    body: {
+        type: String,
+        required: true,
+        trim: true,
+        minlength: 5,
+        mexlength: 5000
+    },
+    category: {
+        _id: mongoose.Schema.Types.ObjectId,
+        name: {
             type: String,
             required: true,
-            trim: true,
-            minlength: 5,
-            mexlength: 60
-        },
-        body: {
-            type: String,
-            required: true,
-            trim: true,
-            minlength: 5,
-            mexlength: 5000
-        },
-        category: {
-            _id: mongoose.Schema.Types.ObjectId,
-            name: {
-                type: String,
-                required: true,
-                minlength: 2,
-                maxlength: 20
-            }
-        },
-        author: {
-            _id: mongoose.Schema.Types.ObjectId,
-            username: {
-                type: String,
-                required: true,
-                trim: true,
-                minlength: 2,
-                maxlength: 30
-            }
+            minlength: 2,
+            maxlength: 20
         }
-    });
+    },
+    author: {
+        _id: mongoose.Schema.Types.ObjectId,
+        username: {
+            type: String,
+            required: true,
+            trim: true,
+            minlength: 2,
+            maxlength: 30
+        }
+    },
+    imgLink: {
+        type: String,
+        trim: true,
+        set: value => {
+            if (value.length === 0) {
+                return "/static/images/bojokogybi1.png";
+            }
+            return value;
+        }
+    },
+    meta: {
+        likes: Number,
+        dislikes: Number,
+        tags: [{
+            _id: mongoose.Schema.Types.ObjectId,
+            name: String
+        }]
+    },
+    comments: [{}]
+});
+newsSchema.set("collection", "News");
+newsSchema.set("timestamps", true);
 
-    let NewsModel = mongoose.model('News', newsSchema);
-}
+mongoose.model("News", newsSchema);
+let newsModel = mongoose.model("News");
+
+module.exports = newsModel;
+
