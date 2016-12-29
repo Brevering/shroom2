1c26c3f5584263f3388e41f146f72ff88c1cc68f
bbojkov <bbojkov90@googlemail.com>
2016-12-29 14:14:59 +0200

    added visualization of the current user on the navbar

--- src/app/common/navbar/navbar.component.ts
+++ src/app/common/navbar/navbar.component.ts
@@ -1,17 +1,22 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
+import { User } from '../../_models/index';
+
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
+  currentUser: User;
   @Input() isHidden: boolean;
   @Output() onToggleCategoryBox = new EventEmitter<boolean>();
 
   private isActive: boolean = false;
 
-  constructor() { }
+  constructor() {
+    this.currentUser = JSON.parse(localStorage.getItem('currentUser'));
+  }
 
   ngOnInit() {
   }
