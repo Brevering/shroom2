60bc320d3a1ef2de394f29b80d93556f352d07fc
pollx <dimova.paola@gmail.com>
2017-01-02 18:54:42 +0200

    added functionality for posts and likes counts

--- server.js
+++ server.js
@@ -53,6 +53,7 @@ apiRoutes.post('/signup', usersController.postRegister);
 apiRoutes.post('/authenticate', usersController.postAuthenticate);
 apiRoutes.get('/users', usersController.getAll);
 
+apiRoutes.get('/profile/counts', usersController.getCounts);
 apiRoutes.get('/profile/likes', usersController.getLikes);
 apiRoutes.post('/profile/posts', usersController.addToUserPosts);
 apiRoutes.get('/users/like', usersController.ifLiked);
