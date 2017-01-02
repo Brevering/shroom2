65618deade0024fd794404d4211724399ba5742f
bbojkov <bbojkov90@googlemail.com>
2017-01-02 18:50:18 +0200

    Merge lost of users controller updateUserProfile method

--- server/data/users.js
+++ server/data/users.js
@@ -1,7 +1,18 @@
 const User = require('mongoose').model('User');
 
 module.exports = {
-    create: function(user, callback) {
+    create: function (user, callback) {
         User.create(user, callback);
+    },
+    update: function (user, callback) {
+        let query = { 'username': user.username };
+
+        User.findOneAndUpdate(query, user, {
+            new: true,
+            upsert: false
+        }, callback);
+    },
+    delete: function (username, callback) {
+        User.findOneAndRemove({ username: username }, callback);
     }
 };
