f255050e90a3e6cf18b42dc28924928c97e86310
Stanimir Stoev <stoevs@gmail.com>
2017-01-07 20:19:00 +0200

    fixed mlab db route

--- server/config/database.js
+++ server/config/database.js
@@ -1,5 +1,5 @@
 module.exports = {
   'secret': 'someawesomesecret',
   //'database': "mongodb://localhost:27017/shrooms"
-  'database': 'mongodb://commodore:pass1@ds159377.mlab.com:59377/infected_mushroom'
+  'database': 'mongodb://commodore:pass1@ds151228.mlab.com:51228/shrooms'
 };
